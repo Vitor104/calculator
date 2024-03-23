06c83db84d665c45ca587cdef77d744e5d18b476
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-03-23 14:55:01 -0300

    criei a parte do display
    agora vou começar a parte lógica

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+
+function apagar() {
+    let displayvisor = document.getElementById("display").innerHTML = 0;
+}
+
+function multiplicacao(num1, num2) {
+    let multiplicar = num1 * num2;
+    let multidisplay = document.getElementById("display").innerHTML = multiplicar;
+    return multidisplay;
+}
+
+
+function visor(num) {
+    let visordisplay = document.getElementById("display").innerHTML = num;
+    return visordisplay;
+}
